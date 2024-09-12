f93d1e4f57f6adf67d0ca35313695b8b1ddd423d
Susmi701 <susmiels590@gmail.com>
2024-09-12 18:04:06 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  enum role: %i[user student staff teacher admin]
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
